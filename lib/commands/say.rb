d0923b064eae4a97e467e711925643aa42ef7d00
Benjamin Manns <benmanns@gmail.com>
2011-05-14 16:24:49 -0400

    Created Say command.

---
+++ lib/commands/say.rb
@@ -0,0 +1,10 @@
+class OgreIsland::Commands::Say < OgreIsland::Commands::Base
+  register 'SAY', :mode => :server do |command|
+    command.string :name
+    command.string :text
+    command.string :channel
+  end
+  register 'SAY', :mode => :client do |command|
+    command.string :text
+  end
+end
